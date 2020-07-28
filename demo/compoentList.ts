ce574d0dcd55d9041f5a1b2b3995bce6dffb596f
252675163 <252675163@qq.com>
2020-07-28 20:01:30 +0800

    feat: useInViewport

--- demo/compoentList.ts
+++ demo/compoentList.ts
@@ -1,1 +1,1 @@
-export default ['useHover', 'useToggle', 'useClickAway', 'useEventListener', 'useReactiveRef', 'useScroll']
+export default ['useHover', 'useToggle', 'useClickAway', 'useEventListener', 'useReactiveRef', 'useScroll', 'useInViewport']
