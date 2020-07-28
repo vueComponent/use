4f38326eb4aafea4189c83c209182d5038fab709
252675163 <252675163@qq.com>
2020-07-28 20:01:30 +0800

    feat: useInViewport

---
+++ packages/useInViewport/__test__/index.spec.tsx
@@ -0,0 +1,27 @@
+import { shallowMount } from '@vue/test-utils';
+import { ref, Ref } from "@vue/runtime-dom"
+import useInViewport from '../index';
+
+describe('useInViewport', () => {
+  it('should be defined', () => {
+    expect(useInViewport).toBeDefined();
+  });
+  // it('with argument', async () => {
+  //   const eleRef = ref(null)
+  //   let inViewPort!: Ref<boolean> | Ref<null>
+  //   const wrapper = shallowMount({
+  //     setup() {
+  //       inViewPort = useInViewport(eleRef);
+  //       return { eleRef };
+  //     },
+  //     render(_ctx) {
+  //       return (
+  //         <h1 ref='eleRef' >
+  //         </h1>
+  //       )
+  //     }
+  //   });
+  //   await wrapper.vm.$nextTick()
+  //   expect(inViewPort.value).toBe(true)
+  // });
+});
