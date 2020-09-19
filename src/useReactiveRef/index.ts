0302ea9c89d7f0d7cb663ca2647d35099e456cf6
tangjinzhou <415800467@qq.com>
2020-09-19 22:34:36 +0800

    fix: useReactiveRef support vue3.0

--- src/useReactiveRef/index.ts
+++ src/useReactiveRef/index.ts
@@ -1,18 +1,12 @@
 import { ref, ComponentPublicInstance, nextTick, Ref } from '@vue/runtime-dom';
 type ElementType = HTMLElement | ComponentPublicInstance;
-function useReactiveRef(isImmediate?: boolean): [Ref<ElementType>, (...args: any) => void] {
+function useReactiveRef(): [Ref<ElementType>, (...args: any) => void] {
   let prevEle = null as ElementType | null;
   const eleRef = ref(prevEle);
   function setEle(ele: HTMLElement) {
     if (prevEle === ele) return;
     prevEle = ele;
-    if (isImmediate) {
-      eleRef.value = prevEle;
-    } else {
-      nextTick(() => {
-        eleRef.value = prevEle;
-      });
-    }
+    eleRef.value = prevEle;
   }
   return [eleRef, setEle];
 }
